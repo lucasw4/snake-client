9f70cdb7bf23e654ed6e728d7de4b09c07f8778b
Lucas Sahota <lucaswade4@gmail.com>
2022-10-19 14:44:35 -0700

    Added seperate file for connection and exported it to play.js

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host: "172.31.118.167",
-    port: 50541,
-  });
-
-  conn.on('data', (data) => {
-    console.log(`Server: ${data}`);
-  });
-
-  conn.setEncoding("utf-8");
-
-  return conn;
-};
+const connect = require('./client');
 
 console.log("Connecting...");
 connect();
