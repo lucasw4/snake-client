f00a396b7efcf7868a8001e29911b000a92924f3
Lucas Sahota <lucaswade4@gmail.com>
2022-10-19 15:12:42 -0700

    added ability to exit game with control C

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
+const process = require('process');
 const connect = require('./client');
 
 console.log("Connecting...");
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === `\u0003`) {
+    process.exit();
+  }
+};
+
+setupInput();
