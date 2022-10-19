1cc19c78b7aff197abd0e427c3ebcd88ed87e6d4
Lucas Sahota <lucaswade4@gmail.com>
2022-10-19 15:28:06 -0700

    Added WASD movement for snake game

---
+++ input.js
@@ -0,0 +1,32 @@
+let conn;
+
+const setupInput = function(connection) {
+  conn = connection;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf-8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === `\u0003`) {
+    process.exit();
+  }
+
+  if (key === 'W' || key == 'w') {
+    conn.write('Move: up');
+  }
+  if (key === 'A' || key === 'a') {
+    conn.write('Move: left');
+  }
+  if (key === 'S' || key === 's') {
+    conn.write('Move: down');
+  }
+  if (key === 'D' || key === 'd') {
+    conn.write('Move: right');
+  }
+};
+
+module.exports = setupInput;
--- play.js
+++ play.js
@@ -1,22 +1,8 @@
-const process = require('process');
 const connect = require('./client');
+const setupInput = require('./input');
 
 console.log("Connecting...");
-connect();
+const conn = connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
 
-const handleUserInput = function(key) {
-  if (key === `\u0003`) {
-    process.exit();
-  }
-};
-
-setupInput();
+setupInput(conn);
